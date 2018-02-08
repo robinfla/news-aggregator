cdb5adcb45ca09aa1fee713f1cf61f6f087be84d
Robin Flamant <robinflamant@Robins-MacBook-Pro-2.local>
2018-02-09 01:44:32 +0700

    adding bootsrap design + reworked hbs

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const hbs = require('express-handlebars');
-const context = require('./routes/index.js');
+const exphbs = require('express-handlebars');
 const news = require('./news.js');
 const MongoClient = require('mongodb').MongoClient;
+
+// setting handlebars options and helpers
+const hbs = exphbs.create({
+    helpers: {
+        dateSincePosted: (dateSince) => {
+            const today = new Date();
+            const dateArticle = new Date(dateSince);
+            const timeInMilisec = today.getTime() - dateArticle.getTime();
+            return (Math.floor(timeInMilisec / (1000 * 60 * 60 * 24)));
+        }
+    },
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir:__dirname + '/views/layouts/'
+});
 
 app.listen(3000, () => {
     console.log("server started");
@@ -13,9 +27,10 @@ app.listen(3000, () => {
 
 // view engine setup //
 app.set('views', path.join(__dirname, 'views'));
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir:__dirname + '/views/layouts'}));
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
+// answering GET request
 app.get('/', async (req, res) => {
     console.log("someone tried to connect");
     try {
@@ -27,6 +42,7 @@ app.get('/', async (req, res) => {
     }
 });
 
+// extracting article from local database
 async function getContext () {
     try {
             const client = await MongoClient.connect('mongodb://localhost:27017/watchDB');
